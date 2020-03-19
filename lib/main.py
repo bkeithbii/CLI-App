efabc00090465ec91e76f74edf51a1e13095c595
Brian Brown II <bkeithbii@gmail.com>
2020-03-19 10:26:52 -0400

    Base/Note Models

--- lib/main.py
+++ lib/main.py
@@ -5,5 +5,17 @@ db = PostgresqlDatabase(
     "notesapp", user="postgres", password="", host="localhost", port=5432
 )
 
+# Define base model for db to be used
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+# Define notes model with properties
+class Notes(BaseModel):
+    title = CharField(max_length=200)
+    body = TextField()
+    date = DateField()
+
 
 db.connect()
