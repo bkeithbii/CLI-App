f88128fcea4d02699c96ba51bac5a9638ce49b29
Brian Brown II <bkeithbii@gmail.com>
2020-03-19 16:03:31 -0400

    Updated modelsx

--- lib/main.py
+++ lib/main.py
@@ -16,16 +16,18 @@ class Notes(BaseModel):
     title = CharField(max_length=200)
     body = TextField()
     date = DateField()
+    username = ForeignKeyField(User, field="username")
 
 
 # Define user model (will see if this works later)
 class User(BaseModel):
     first_name = CharField()
-    username = CharField(unique=True)
+    username = ForeignKeyField(unique=True)
 
 
 # Create tables for  Notes/User models
 db.create_tables([Notes])
 db.create_tables([User])
+
 
 db.connect()
